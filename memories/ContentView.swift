d1fe976f3115d51210551882ccc6ac154cc92e3c
alina-potapova <alineus@mail.ru>
2021-07-08 12:52:24 +0300

    Initial Commit

---
+++ memories/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  memories
+//
+//  Created by Alina Potapova on 08.07.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ memories/memoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  memoriesApp.swift
+//  memories
+//
+//  Created by Alina Potapova on 08.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct memoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
