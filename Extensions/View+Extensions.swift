fa4f27eb4c7be30d8e1ffc664397ac9c382a9c28
alina-potapova <alineus@mail.ru>
2021-12-13 17:41:47 +0300

    commit

--- Extensions/View+Extensions.swift
+++ Extensions/View+Extensions.swift
@@ -43,6 +43,7 @@ extension View {
         controller.view.frame = CGRect(x: 0, y: CGFloat(Int.max), width: 1, height: 1)
         UIApplication.shared.windows.first!.rootViewController?.view.addSubview(controller.view)
         
+        
         let size = controller.sizeThatFits(in: UIScreen.main.bounds.size)
         controller.view.bounds = CGRect(origin: .zero, size: size)
         controller.view.sizeToFit()
