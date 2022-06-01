7d5173e7cffead42cf7648867f1ab5bea7c5edb3
pamapp <alineus2001@yandex.ru>
2022-06-01 12:24:24 +0300

    Add unit tests

--- memoriesTests/CoreDataStack.swift
+++ memoriesTests/CoreDataStack.swift
@@ -1,68 +1,100 @@
-////
-////  CoreDataStack.swift
-////  memoriesTests
-////
-////  Created by Alina Potapova on 14.12.2021.
-////
 //
-//import CoreData
+//  CoreDataStack.swift
+//  memoriesTests
 //
-//open class CoreDataStack {
-//    public static let modelName = "Memories"
+//  Created by Alina Potapova on 14.12.2021.
 //
-//    public lazy var model: NSManagedObjectModel = {
-//        let modelURL = Bundle.main.url(forResource: CoreDataStack.modelName, withExtension: "momd")!
-//        return NSManagedObjectModel(contentsOf: modelURL)!
-//    }()
-//
-//    public lazy var mainContext: NSManagedObjectContext = {
-//        return storeContainer.viewContext
-//    }()
-//
-//    public lazy var storeContainer: NSPersistentContainer = {
-//        let container = NSPersistentContainer(name: CoreDataStack.modelName, managedObjectModel: model)
-//        container.loadPersistentStores { _, error in
-//            if let error = error as NSError? {
-//                fatalError("Unresolved error \(error), \(error.userInfo)")
-//            }
-//        }
-//        return container
-//    }()
-//
-//    public func newDerivedContext() -> NSManagedObjectContext {
-//        let context = storeContainer.newBackgroundContext()
-//        return context
-//    }
-//
-//    public func saveContext() {
-//        saveContext(mainContext)
-//    }
-//
-//    public func saveContext(_ context: NSManagedObjectContext) {
-//        if context != mainContext {
-//            saveDerivedContext(context)
-//            return
-//        }
-//
-//        context.perform {
-//            do {
-//                try context.save()
-//            } catch let error as NSError {
-//                fatalError("Unresolved error \(error), \(error.userInfo)")
-//            }
-//        }
-//    }
-//
-//    public func saveDerivedContext(_ context: NSManagedObjectContext) {
-//        context.perform {
-//            do {
-//                try context.save()
-//            } catch let error as NSError {
-//                fatalError("Unresolved error \(error), \(error.userInfo)")
-//            }
-//
-//            self.saveContext(self.mainContext)
-//        }
-//    }
-//}
-//
+import Foundation
+import CoreData
+import memories
+
+open class CoreDataStack {
+  public static let modelName = "Commentarium"
+
+  public static let model: NSManagedObjectModel = {
+    // swiftlint:disable force_unwrapping
+    let modelURL = Bundle.main.url(forResource: modelName, withExtension: "momd")!
+    return NSManagedObjectModel(contentsOf: modelURL)!
+  }()
+  // swiftlint:enable force_unwrapping
+
+  public init() {
+  }
+
+  public lazy var mainContext: NSManagedObjectContext = {
+    return storeContainer.viewContext
+  }()
+
+  public lazy var storeContainer: NSPersistentContainer = {
+    let container = NSPersistentContainer(name: CoreDataStack.modelName, managedObjectModel: CoreDataStack.model)
+    container.loadPersistentStores { _, error in
+      if let error = error as NSError? {
+        fatalError("Unresolved error \(error), \(error.userInfo)")
+      }
+    }
+    return container
+  }()
+
+  public func newDerivedContext() -> NSManagedObjectContext {
+    let context = storeContainer.newBackgroundContext()
+    return context
+  }
+
+  public func saveContext() {
+    saveContext(mainContext)
+  }
+
+  public func saveContext(_ context: NSManagedObjectContext) {
+    if context != mainContext {
+      saveDerivedContext(context)
+      return
+    }
+
+    context.perform {
+      do {
+        try context.save()
+      } catch let error as NSError {
+        fatalError("Unresolved error \(error), \(error.userInfo)")
+      }
+    }
+  }
+
+  public func saveDerivedContext(_ context: NSManagedObjectContext) {
+    context.perform {
+      do {
+        try context.save()
+      } catch let error as NSError {
+        fatalError("Unresolved error \(error), \(error.userInfo)")
+      }
+
+      self.saveContext(self.mainContext)
+    }
+  }
+}
+
+class TestCoreDataStack: CoreDataStack {
+  override init() {
+    super.init()
+
+    // 1
+    let persistentStoreDescription = NSPersistentStoreDescription()
+    persistentStoreDescription.type = NSInMemoryStoreType
+
+    // 2
+    let container = NSPersistentContainer(
+      name: CoreDataStack.modelName,
+      managedObjectModel: CoreDataStack.model)
+
+    // 3
+    container.persistentStoreDescriptions = [persistentStoreDescription]
+
+    container.loadPersistentStores { _, error in
+      if let error = error as NSError? {
+        fatalError("Unresolved error \(error), \(error.userInfo)")
+      }
+    }
+
+    // 4
+    storeContainer = container
+  }
+}
--- memoriesTests/memoriesTests.swift
+++ memoriesTests/memoriesTests.swift
@@ -1,96 +1,201 @@
-////
-////  memoriesTests.swift
-////  memoriesTests
-////
-////  Created by Alina Potapova on 07.12.2021.
-////
-//
-//import XCTest
-//import CoreData
-//@testable import memories
-//
-//public class TestCoreDataStack: CoreDataStack {
-//  public override init() {
-//    super.init()
-//
-//    let persistentStoreDescription = NSPersistentStoreDescription()
-//    persistentStoreDescription.type = NSInMemoryStoreType
-//
-//    let container = NSPersistentCloudKitContainer(name: "Memories")
-//
-//    container.persistentStoreDescriptions = [persistentStoreDescription]
-//
-//    container.loadPersistentStores { _, error in
-//      if let error = error as NSError? {
-//        fatalError("Unresolved error \(error), \(error.userInfo)")
-//      }
-//    }
-//
-//    storeContainer = container
-//  }
-//}
-//
-//
-//
-//class memoriesTests: XCTestCase {
-//
-//    var coreDataStack: CoreDataStack!
-//
-//    lazy var context = self.coreDataStack.mainContext
-//
-////    var model = FoldersView.FolderModel.init(moc: context)
-//
-//    override func setUp() {
-//        coreDataStack = TestCoreDataStack()
-//    }
-//
-//    override func tearDown() {
-//      super.tearDown()
-//      coreDataStack = nil
-//    }
-//
-//    func testExample() throws {
-//        // This is an example of a functional test case.
-//        // Use XCTAssert and related functions to verify your tests produce the correct results.
-//    }
-//
-//    func testPerformanceExample() throws {
-//        // This is an example of a performance test case.
-//        measure {
-//            // Put the code you want to measure the time of here.
-//        }
-//    }
-//
-//    func testAddFolder() {
-//        let folder = Folder(context: context)
-//        folder.name = "TestFolder"
-//        folder.isFavorite = false
-//        folder.id = UUID()
-//        XCTAssertEqual(folder.name, "TestFolder")
-//        XCTAssertEqual(folder.isFavorite, false)
-//        XCTAssertNotNil(folder.id, "id should not be nil")
-//    }
-//
-//    func testAddMemory() {
-//        let folder = Folder(context: context)
-//        folder.name = "TestFolder"
-//        folder.isFavorite = false
-//        folder.id = UUID()
-//
-//        let memory = Memory(context: context)
-//        memory.content = "Something"
-//        memory.place = "Somewhere"
-//        memory.date = Date()
-//        memory.isFavorite = false
-//        memory.id = UUID()
-//        memory.is_in = folder
-//
-//        XCTAssertEqual(memory.content, "Something")
-//        XCTAssertEqual(memory.place, "Somewhere")
-//        XCTAssertEqual(folder.isFavorite, false)
-//        XCTAssertNotNil(memory.id, "id should not be nil")
-//        XCTAssertEqual(memory.is_in, folder)
-//    }
-//
-//}
-//
+import XCTest
+import SwiftUI
+@testable import memories
+import CoreData
+
+class FolderTests: XCTestCase {
+    // MARK: - Properties
+    // swiftlint:disable implicitly_unwrapped_optional
+    var folders: FoldersView.FolderModel!
+    var coreDataStack: CoreDataStack!
+    // swiftlint:enable implicitly_unwrapped_optional
+
+    override func setUp() {
+        super.setUp()
+        coreDataStack = TestCoreDataStack()
+        folders = FoldersView.FolderModel.init(moc: coreDataStack.mainContext)
+    }
+
+    override func tearDown() {
+        super.tearDown()
+        folders = nil
+        coreDataStack = nil
+    }
+
+    func testDefaultFolder() {
+        let folder = folders.getDefaultFolder()
+        XCTAssertNotNil(folder, "Folder should not be nil")
+        XCTAssertNotNil(folder.id, "Id should not be nil")
+    }
+    
+    func testAddFolder() {
+        folders.addNewFolder(name: "Test", isFav: false, color: 1)
+        XCTAssertNotNil(folders.folders[0], "Folder should not be nil")
+        XCTAssertNotNil(folders.folders[0].id, "Id should not be nil")
+        XCTAssertTrue(folders.folders[0].name == "Test")
+    }
+    
+    func testRemoveFolder() {
+        folders.addNewFolder(name: "Test", isFav: false, color: 1)
+        XCTAssertNotNil(folders.folders[0].id, "Id should not be nil")
+        folders.removeFolder(folder: folders.folders[0])
+        XCTAssertTrue(folders.folders.isEmpty)
+    }
+    
+    func testEditFolder() {
+        folders.addNewFolder(name: "Test1", isFav: false, color: 1)
+        XCTAssertTrue(folders.folders[0].name == "Test1")
+        XCTAssertTrue(folders.folders[0].isFavorite == false)
+        XCTAssertTrue(folders.folders[0].color == 1)
+        
+        folders.editFolder(folder: folders.folders[0], name: "Test2", isFav: true, color: 2)
+        XCTAssertTrue(folders.folders[0].name == "Test2")
+        XCTAssertTrue(folders.folders[0].isFavorite == true)
+        XCTAssertTrue(folders.folders[0].color == 2)
+    }
+}
+
+
+class MemoryTests: XCTestCase {
+    // MARK: - Properties
+    // swiftlint:disable implicitly_unwrapped_optional
+    var testFolder: Folder!
+    var testLocation: Location!
+    var inputImage: UIImage!
+    var memories: MemoriesView.MemoryModel!
+    var coreDataStack: CoreDataStack!
+    // swiftlint:enable implicitly_unwrapped_optional
+
+    override func setUp() {
+        super.setUp()
+        inputImage = UIImage(systemName: "cloud")
+        coreDataStack = TestCoreDataStack()
+        testFolder = FoldersView.FolderModel.init(moc: coreDataStack.mainContext).getDefaultFolder()
+        testLocation = LocationSelectView.LocationModel.init(moc: coreDataStack.mainContext).getDefaultLocation()
+        memories = MemoriesView.MemoryModel.init(moc: coreDataStack.mainContext, folder: testFolder)
+    }
+
+    override func tearDown() {
+        super.tearDown()
+        inputImage = nil
+        memories = nil
+        testFolder = nil
+        testLocation = nil
+        coreDataStack = nil
+    }
+    
+    func testAddMemory() {
+        memories.addNewMemory(title: "Memory", place: testLocation, text: "Something", date: Date(), folder: testFolder, image: inputImage, color: 1)
+        XCTAssertNotNil(memories.memories[0], "Memory should not be nil")
+        XCTAssertNotNil(memories.memories[0].id, "Id should not be nil")
+    }
+
+    func testRemoveMemory() {
+        memories.addNewMemory(title: "Memory", place: testLocation, text: "Something", date: Date(), folder: testFolder, image: inputImage, color: 1)
+        XCTAssertNotNil(memories.memories[0].id, "Id should not be nil")
+        memories.removeMemory(memory: memories.memories[0])
+        XCTAssertTrue(memories.memories.isEmpty)
+    }
+
+    func testEditMemory() {
+        memories.addNewMemory(title: "Memory1", place: testLocation, text: "Something1", date: Date(), folder: testFolder, image: inputImage, color: 1)
+        XCTAssertTrue(memories.memories[0].title == "Memory1")
+        XCTAssertTrue(memories.memories[0].content == "Something1")
+        XCTAssertTrue(memories.memories[0].color == 1)
+
+        memories.editMemory(memory: memories.memories[0], text: "Something2", title: "Memory2", place: testLocation, date: Date(), folder: testFolder, color: 2, image: inputImage)
+        XCTAssertTrue(memories.memories[0].title == "Memory2")
+        XCTAssertTrue(memories.memories[0].content == "Something2")
+        XCTAssertTrue(memories.memories[0].color == 2)
+    }
+}
+
+
+class LocationTests: XCTestCase {
+    // MARK: - Properties
+    // swiftlint:disable implicitly_unwrapped_optional
+    var locations: LocationSelectView.LocationModel!
+    var coreDataStack: CoreDataStack!
+    // swiftlint:enable implicitly_unwrapped_optional
+
+    override func setUp() {
+        super.setUp()
+        coreDataStack = TestCoreDataStack()
+        locations = LocationSelectView.LocationModel.init(moc: coreDataStack.mainContext)
+    }
+
+    override func tearDown() {
+        super.tearDown()
+        locations = nil
+        coreDataStack = nil
+    }
+    
+    func testAddLocation() {
+        let location = locations.addLocation(name: "Test", longitude: 0.0, latitude: 0.0)
+        XCTAssertTrue(locations.locations[0] == location)
+        XCTAssertNotNil(location, "Location should not be nil")
+        XCTAssertNotNil(location.id, "Id should not be nil")
+    }
+
+    func testRemoveLocation() {
+        let location = locations.addLocation(name: "Test", longitude: 0.0, latitude: 0.0)
+        XCTAssertTrue(locations.locations[0] == location)
+        locations.removeLocation(location: location)
+        XCTAssertTrue(locations.locations.isEmpty)
+    }
+
+    func testChangeLocationCoordinates() {
+        var location = locations.addLocation(name: "Test1", longitude: 0.0, latitude: 0.0)
+        XCTAssertTrue(locations.locations[0].name == "Test1")
+        XCTAssertTrue(locations.locations[0].longitude == 0.0)
+        XCTAssertTrue(locations.locations[0].latitude == 0.0)
+
+        location = locations.changeLocationCoordinates(location: location, name: "Test2", longitude: 0.1, latitude: 0.1)
+        XCTAssertTrue(locations.locations[0].name == "Test2")
+        XCTAssertTrue(locations.locations[0].longitude == 0.1)
+        XCTAssertTrue(locations.locations[0].latitude == 0.1)
+        
+    }
+}
+
+
+class UserTests: XCTestCase {
+    // MARK: - Properties
+    // swiftlint:disable implicitly_unwrapped_optional
+    var userData: ProfileView.UserModel!
+    var coreDataStack: CoreDataStack!
+    var inputImage: UIImage!
+    var user: User!
+    // swiftlint:enable implicitly_unwrapped_optional
+
+    override func setUp() {
+        super.setUp()
+        inputImage = UIImage(systemName: "cloud")
+        coreDataStack = TestCoreDataStack()
+        userData = ProfileView.UserModel.init(moc: coreDataStack.mainContext)
+        user = userData.getDefaultUser()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+        inputImage = nil
+        user = nil
+        userData = nil
+        coreDataStack = nil
+    }
+    
+    func testDefaultUser() {
+        XCTAssertNotNil(user, "User should not be nil")
+        XCTAssertNotNil(user.id, "Id should not be nil")
+        XCTAssertTrue(user.name == "User")
+    }
+    
+    func testEditUserData() {
+        userData.editUserData(user: user, name: "Test", image: inputImage)
+        XCTAssertNotNil(user, "User should not be nil")
+        XCTAssertNotNil(user.id, "Id should not be nil")
+        XCTAssertTrue(user.name == "Test")
+    }
+
+}
+
